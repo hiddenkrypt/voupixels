22b51e9480e93ee5905bfb71f8b071031db55056
Ada Clark <hiddenkrypt@gmail.com>
2021-10-13 22:03:19 -0700

    adding an intial take on a render and update loop, with a simiple ball sim just to try things out.

--- Engine.js
+++ Engine.js
@@ -11,6 +11,7 @@ window.onload = function engine() {
     var spritesheet = loader.loadImage("spritesheet.png");
 	loader.onComplete = function loadingComplete() {
         console.log("loader finished");
+		requestAnimationFrame( renderLoop );
         ctx.fillStyle = "#d0c0b0";
         ctx.fillRect(10, 10, 10, 10);
         ctx.drawImage(spritesheet, 0, 0, 700, 700);
@@ -18,4 +19,31 @@ window.onload = function engine() {
         console.log(spritesheet);
     };
 	loader.begin();
+	var ball = {x:0, y:0, dx:1, dy:1}
+	function renderLoop(){
+		updateEntities();
+		ctx.clearRect(0,0,1000,600);
+		
+		
+        ctx.fillStyle = "#00cc00";
+        ctx.fillRect(ball.x+5, 0, 1, canvas.height);
+        ctx.fillRect(0, ball.y+5, canvas.width, 1);
+		
+        ctx.fillStyle = "#d0c0b0";
+        ctx.fillRect(ball.x, ball.y, 10, 10);
+		
+		requestAnimationFrame( renderLoop );
+	}
+	
+	function updateEntities(){
+		ball.x += ball.dx;
+		ball.y += ball.dy;
+		if(ball.x > canvas.width){ ball.dx = -1; }
+		if(ball.x < 0){ ball.dx = 1; }
+		if(ball.y > canvas.height){ ball.dy = -1; }
+		if(ball.y < 0){ ball.dy = 1; }
+	}
+
+
 };
+
